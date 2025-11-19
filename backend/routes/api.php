aecf6758ff14fcdf42ca8b5aaf1e2af5d452bce9
Jan Ferencak <janik.ferencak3@gmail.com>
2025-11-19 19:57:22 +0100

    Pridane endpointu pre kontrolu API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,5 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PingController;
+use App\Models\Ping;
 
 Route::post('/ping', [PingController::class, 'store']);
+
+// jednoduchý výpis všetkých pingov
+Route::get('/pings', function () {
+    return Ping::all();
+});
